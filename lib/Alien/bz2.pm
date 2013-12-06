bc18ffe29b75535dfb5eed8ae584cfd49b6eedbb
Graham Ollis <perl@wdlabs.com>
2013-12-06 08:15:22 -0500

    use File::Spec

--- lib/Alien/bz2.pm
+++ lib/Alien/bz2.pm
@@ -4,12 +4,14 @@ use strict;
 use warnings;
 use base qw( Alien::Base );
 use File::ShareDir qw( dist_dir );
+use File::Spec;
 
 # ABSTRACT: Build and make available libbz2
 # VERSION
 
-sub cflags { '-I' . dist_dir('Alien-bz2') . '/include'   }
-sub libs   { '-L' . dist_dir('Alien-bz2') . '/lib -lbz2' }
+sub cflags   { '-I' . File::Spec->catdir(dist_dir('Alien-bz2'), 'include')        }
+sub libs     { '-L' . File::Spec->catdir(dist_dir('Alien-bz2'), 'lib') . ' -lbz2' }
+sub dll_path { File::Spec->catfile(dist_dir('Alien-bz2'), qw( bin bz2.dll ))      }
 
 # workaround for Alien::Base gh#30
 sub import
@@ -21,6 +23,11 @@ sub import
     unshift @DynaLoader::dl_library_path, 
       grep { s/^-L// } 
       shellwords( $class->libs );
+  }
+  
+  if($^O eq 'MSWin32')
+  {
+    $ENV{PATH} = dist_dir("Alien-bz2") . "\\bin;$ENV{PATH}";
   }
   
   $class->SUPER::import(@_);
