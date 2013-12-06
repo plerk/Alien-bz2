6de22225637a9fd5d63d148d4c74e0c44438f285
Graham Ollis <perl@wdlabs.com>
2013-12-05 21:49:57 -0500

    initial version

---
+++ inc/prep.pl
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+
+system 'wget', 'http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz' unless -e 'bzip2-1.0.6.tar.gz';
--- lib/Alien/bz2.pm
+++ lib/Alien/bz2.pm
@@ -2,9 +2,13 @@ package Alien::bz2;
 
 use strict;
 use warnings;
-use v5.10;
+use base qw( Alien::Base );
+use File::ShareDir qw( dist_dir );
 
 # ABSTRACT: Build and make available libbz2
 # VERSION
+
+sub cflags { '-I' . dist_dir('Alien-bz2') . '/include'   }
+sub libs   { '-L' . dist_dir('Alien-bz2') . '/lib -lbz2' }
 
 1;
