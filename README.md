b0971611a1ffe81b6d48f2967be4a496585fa9a1
Graham Ollis <perl@wdlabs.com>
2013-12-06 08:22:16 -0500

    documentation

--- README.md
+++ README.md
@@ -2,6 +2,52 @@
 
 Build and make available libbz2
 
+# SYNOPSIS
+
+Build.PL
+
+    use Alien::bz2;
+    use Module::Build;
+    
+    my $alien = Alien::bz2->new;
+    my $build = Module::Build->new(
+      ...
+      extra_compiler_flags => $alien->cflags,
+      extra_linker_flags   => $alien->libs,
+      ...
+    );
+    
+    $build->create_build_script
+
+Makefile.PL
+
+    use Alien::bz2
+    use ExtUtils::MakeMaker;
+    
+    my $alien = Alien::bz2->new;
+    WriteMakefile(
+      ...
+      CFLAGS => Alien::bz2->cflags,
+      LIBS   => Alien::bz2->libs,
+    );
+
+FFI
+
+    use Alien::bz2;
+    use FFI::Sweet qw( ffi_lib );
+    
+    ffi_lib(Alien::bz2->new->libs);
+
+# DESCRIPTION
+
+This distribution installs the bzip2 libraries and makes them available [Alien::Base](https://metacpan.org/pod/Alien::Base) style.
+
+Unless you have specific need for this, you probably want [Compress::Bzip2](https://metacpan.org/pod/Compress::Bzip2).
+
+# SEE ALSO
+
+- [Compress::Bzip2](https://metacpan.org/pod/Compress::Bzip2)
+
 # AUTHOR
 
 Graham Ollis <plicease@cpan.org>
--- lib/Alien/bz2.pm
+++ lib/Alien/bz2.pm
@@ -10,6 +10,58 @@ use File::Spec;
 # ABSTRACT: Build and make available libbz2
 # VERSION
 
+=head1 SYNOPSIS
+
+Build.PL
+
+ use Alien::bz2;
+ use Module::Build;
+ 
+ my $alien = Alien::bz2->new;
+ my $build = Module::Build->new(
+   ...
+   extra_compiler_flags => $alien->cflags,
+   extra_linker_flags   => $alien->libs,
+   ...
+ );
+ 
+ $build->create_build_script
+
+Makefile.PL
+
+ use Alien::bz2
+ use ExtUtils::MakeMaker;
+ 
+ my $alien = Alien::bz2->new;
+ WriteMakefile(
+   ...
+   CFLAGS => Alien::bz2->cflags,
+   LIBS   => Alien::bz2->libs,
+ );
+
+FFI
+
+ use Alien::bz2;
+ use FFI::Sweet qw( ffi_lib );
+ 
+ ffi_lib(Alien::bz2->new->libs);
+
+=head1 DESCRIPTION
+
+This distribution installs the bzip2 libraries and makes them available L<Alien::Base> style.
+
+Unless you have specific need for this, you probably want L<Compress::Bzip2>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Compress::Bzip2>
+
+=back
+
+=cut
+
 sub cflags   { '-I' . File::Spec->catdir(dist_dir('Alien-bz2'), 'include')        }
 sub libs     { '-L' . File::Spec->catdir(dist_dir('Alien-bz2'), 'lib') . ' -lbz2' }
 sub dll_path { File::Spec->catfile(dist_dir('Alien-bz2'), qw( bin bz2.dll ))      }
